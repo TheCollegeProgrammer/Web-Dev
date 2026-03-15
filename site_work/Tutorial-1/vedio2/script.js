bb378b5d9944623d17b568109afad1b5bd8cbb8b
Devansh Avchat <devanshavchat8208@gmail.com>
2026-03-15 09:15:15 +0530

    removed and created new one

--- site_work/Tutorial-1/vedio2/script.js
+++
@@ -1,8 +0,0 @@
-let text = document.querySelector("#text1");
-
-let sel = document.querySelector("select");
-
-sel.addEventListener("change", function(dets){
-    // console.log(dets.target.value);
-    text.textContent = `${dets.target.value} is Selected`;
-});
--- site_work/app/enquiry_form/page.js
+++
@@ -1,9 +0,0 @@
-import EnquiryForm from "@/components/enquiry_form"
-
-export default function EnquiryPage() {
-  return (
-    <div>
-      <EnquiryForm />
-    </div>
-  )
-}
