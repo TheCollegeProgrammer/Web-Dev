3b614759a1b5a1aa25890ab60080bef340f65c65
Devansh Avchat <devanshavchat8208@gmail.com>
2026-04-27 23:55:00 +0530

    Pushed Updates

--- React-Tutorial/Project-2/src/Components/Left_Text.jsx
+++ React-Tutorial/Project-2/src/Components/Left_Text.jsx
@@ -2,13 +2,15 @@ import React from 'react'
 
 const Left_Text = () => {
   return (
+    <>
     <div className='h-100% w-2/7 pl-1.5 ml-20'>
       <h1 className='text-5xl font-bold font-sans leading-14'>Prospective <br/><span> customer </span> <br/> segmentation</h1>
       <h1>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fugiat, ipsam! Corporis, excepturi sunt maxime doloribus, odit ipsam in, expedita eum consequuntur id animi esse molestias itaque earum laboriosam! Hic, nam.</h1>
+    </div>
       <div className='text-9xl'>
         <i className="ri-arrow-right-up-line"></i>
-      </div>
     </div>
+    </>
   )
 }
 
---
+++ React-Tutorial/Project-2/src/Components/Right_card.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Right_card = () => {
+  return (
+      <div className='h-full w-1/3 bg-blue-400 m-6'>
+      </div>
+  )
+}
+
+export default Right_card
