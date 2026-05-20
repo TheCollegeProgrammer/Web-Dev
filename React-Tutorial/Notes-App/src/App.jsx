06b7deedd5576c8f4eb864fc7b5ada0c95a24b7f
Devansh Avchat <devanshavchat8208@gmail.com>
2026-05-21 00:57:11 +0530

    Updating Files

--- React-Tutorial/Notes-App/src/App.jsx
+++ React-Tutorial/Notes-App/src/App.jsx
@@ -8,12 +8,12 @@ const App = () => {
   }
 
   return (
-    <div className='h-screen bg-black text-white'>
-      <form onSubmit={(e) =>{
+    <div className='h-screen lg:flex bg-black text-white'>
+      <form onSubmit={(e) =>{e
         SubmitHandler(e)
-      }} className='flex justify-between items-start p-10 ml-10'>
-        <div className='flex flex-col items-start w-1/2 gap-4'>
-
+      }} className='flex items-start p-10 ml-10 lg:w-1/2'>
+        <div className='flex flex-col items-start w-full ml-5 mr-5 gap-4'>
+        <h1 className='text-3xl font-bold'>Your Notes</h1>
         <input 
         type='text' 
         placeholder='Enter Notes Heading' 
@@ -29,9 +29,17 @@ const App = () => {
 
         <button className='bg-white text-black px-5 py-2 w-full rounded-2 font-extrabold outline-none'>Add Notes</button>
         </div>
-        <img className='h-100' src='https://file.aiquickdraw.com/imgcompressed/img/compressed_8491c5857e7491d29a7561ac118b13cc.webp'></img>
+        {/* <img className='h-100' src='https://file.aiquickdraw.com/imgcompressed/img/compressed_8491c5857e7491d29a7561ac118b13cc.webp'></img> */}
         
       </form>
+      <div className="lg:w-1/2 p-10 lg:border-l-2">
+        <h1 className='text-3xl font-bold'>Your Notes</h1>
+        <div className="flex gap-5 flex-wrap mt-5 h-full overflow-auto">
+            <div className="h-52 w-40 rounded-2xl bg-white"></div>
+            <div className="h-52 w-40 rounded-2xl bg-white"></div>
+            <div className="h-52 w-40 rounded-2xl bg-white"></div>
+        </div>
+      </div>
     </div>
   )
 }
